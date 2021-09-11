51d1cfe0e9d771689c707d1f22334bfa39505c3d
Alexander Medeiros <amedeiros@soundexchange.com>
2021-09-11 15:46:51 -0400

    remove verbose log

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -98,12 +98,9 @@ const resolvers = {
             return {token, user}
         },
         addOrder: async (parent, {products}, context) => {
-            console.log(context)
             if (context.user) {
                 const order = new Order({products})
-
                 await User.findByIdAndUpdate(context.user._id, {$push: {orders: order}})
-
                 return order
             }
 
